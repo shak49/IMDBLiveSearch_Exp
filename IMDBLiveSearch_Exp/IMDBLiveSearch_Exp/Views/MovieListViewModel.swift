e3f8e1846b7eb5d75d453917fa1af00b18d651bf
Shak <shakfeizi@gmail.com>
2021-10-27 21:54:51 -0600

    ViewModel Completed

--- IMDBLiveSearch_Exp/IMDBLiveSearch_Exp/Views/MovieListViewModel.swift
+++ IMDBLiveSearch_Exp/IMDBLiveSearch_Exp/Views/MovieListViewModel.swift
@@ -6,3 +6,39 @@
 //
 
 import Foundation
+
+
+struct MovieViewModel {
+    // SHAK: Properties
+    let movie: Movie
+    var imdbID: String {
+        movie.imdbID
+    }
+    var title: String {
+        movie.title
+    }
+    var poster: URL? {
+        URL(string: movie.poster)
+    }
+}
+
+@MainActor
+class MovieListViewModel: ObservableObject {
+    // SHAK: Properties
+    var networkCall: NetworkCall = NetworkCall()
+    @Published var movies: [MovieViewModel] = []
+    
+    // SHAK: Functions
+    func search(term: String) async {
+        do {
+            let searchResult = try await networkCall.fetchMovies(searchTerm: term)
+            self.movies = searchResult.map(MovieViewModel.init)
+        } catch {
+            print(error)
+        }
+    }
+}
+
+
+
+
