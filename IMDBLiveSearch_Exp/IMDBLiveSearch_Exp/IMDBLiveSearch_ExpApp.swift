87e3b446096406fcb5124299175cb0c42780a846
Shak <shakfeizi@gmail.com>
2021-10-27 20:13:18 -0600

    First Initialization

---
+++ IMDBLiveSearch_Exp/IMDBLiveSearch_Exp/IMDBLiveSearch_ExpApp.swift
@@ -0,0 +1,17 @@
+//
+//  IMDBLiveSearch_ExpApp.swift
+//  IMDBLiveSearch_Exp
+//
+//  Created by Shak Feizi on 10/27/21.
+//
+
+import SwiftUI
+
+@main
+struct IMDBLiveSearch_ExpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
